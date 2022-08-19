441d0e752bcdca2e52cd1c9f53c99683638c9e5b
William Ferebee <williampferebee@gmail.com>
2022-08-19 16:47:06 -0400

    Changed client object variables to connect to deployed heroku GQL server

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
-  cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
+  uri: 'https://server-catstronauts-wpf.herokuapp.com/',
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
